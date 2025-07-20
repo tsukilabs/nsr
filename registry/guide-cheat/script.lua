bd2256664a32535b2c45700f3fb455cb4d130dc0
Andrew Ferreira <andrew.shien2@gmail.com>
2025-07-20 12:34:37 -0300

    feat: update cheats

--- registry/guide-cheat/script.lua
+++ registry/guide-cheat/script.lua
@@ -1,7 +1,27 @@
 -- Copyright (C) Call of Nil contributors
 -- SPDX-License-Identifier: AGPL-3.0-only
 
---- Sets the level of a building in a village you own.
+--- Gets the amount of resources that a bot has.
+-- @param {number} bot_id
+-- @return {table}
+world:cheat_get_bot_resources(1)
+
+--- Gets the total storage capacity of a bot.
+-- @param {number} bot_id
+-- @return {table}
+world:cheat_get_bot_storage_capacity(1)
+
+--- Gets the amount of resources that a precursor has.
+-- @param {string} precursor_id
+-- @return {table}
+world:cheat_get_precursor_resources("A")
+
+--- Gets the total storage capacity of a precursor.
+-- @param {string} precursor_id
+-- @return {table}
+world:cheat_get_precursor_storage_capacity("A")
+
+--- Sets the level of a building.
 -- @param {table} coord
 -- @param {string} building_id
 -- @param {number} level
@@ -18,7 +38,7 @@ world:cheat_set_iron(10000)
 --- Sets your food to the maximum amount possible.
 world:cheat_set_max_food()
 
---- Sets the levels of all buildings in a village you own to their maximum.
+--- Sets the levels of all buildings in a village to their maximum.
 -- @param {table} coord
 world:cheat_set_max_infrastructure({ x = 0, y = 0 })
 
@@ -44,7 +64,7 @@ world:cheat_set_max_wood()
 -- @param {table} resources
 world:cheat_set_resources({ food = 1000, iron = 1000, stone = 1000, wood = 1000 })
 
---- Sets the stability of a village you own.
+--- Sets the stability of a village.
 -- @param {table} coord
 -- @param {number} stability
 world:cheat_set_stability({ x = 0, y = 0 }, 0.5)
@@ -56,3 +76,7 @@ world:cheat_set_stone(10000)
 --- Sets the amount of wood you have.
 -- @param {number} wood
 world:cheat_set_wood(10000)
+
+--- Spawns a bot.
+-- @return {number} Bot id.
+world:cheat_spawn_bot()
